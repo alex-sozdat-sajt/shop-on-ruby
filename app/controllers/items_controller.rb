bc837bd9b7ea78903eb6a36e0439584eb35bc18c
Alex <schetchik2016@yandex.ru>
2019-02-09 09:30:11 +0300

    RESTful

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,14 +4,33 @@ class ItemsController < ApplicationController
     render text: @items.map {|i| "#{i.name}: #{i.price}"}.join("<br/>")
   end
 
+
+  #/item/1 GET
+  def show
+  end
+
+  #/item/new GET
+  def new
+  end
+
+  #/item/1/edit GET
+  def edit
+  end
+
+#/item POST
 def   create
-#@item = Item.create(name: params[:name], description: params[:description], price: params[:price],
-  real: params[:real], weight: params[:weight])
+#@item = Item.create(name: params[:name], description: params[:description], price: params[:price], real: params[:real], weight: params[:weight])
+
 @item = Item.create(params[:item])
-
   render text: "#{@item.id}: #{@item.name} (#{!@item.new_record?})"
+end
 
+#/item/1 PUT
+def update
+end
 
+#/item/1 DELETE
+def destroy
 end
 
 end
